cf40f4d763ca7da1e94b8de0e82da8642a3dfb71
Yazid SOUMANOU <soumanouyazid0@gmail.com>
2025-01-01 02:17:27 +0100

    affichage des message et debut des class Message

--- class/Message.php
+++ class/Message.php
@@ -14,6 +14,24 @@
         {
             return mb_strlen($this->username,"UTF-8")>=3 || mb_strlen($this->message,"UTF-8")>=10
         }
-        public function
+        public function toHtml () : string
+        {
+            return<<<HTML
+            <P>
+                <strong>$this->username</strong> <em> date('D d M o / H : i : s',$this->date) </em>  <br>
+                $this->message;
+            </P>
+HTML
+        }
+        public function toJson()
+        {
+            $tabMessage = [
+                'username' =>  $this->username,
+                'message' =>  $this->message,
+                'date' => $this->date
+                ];
+            return json_encode($tabMessage);
+        }
+        public static function Mess ()
     }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . "inclus" . DIRECTORY_SEPARATOR ."head.php";
+$file = __DIR__ . DIRECTORY_SEPARATOR . "fichiers" .DIRECTORY_SEPARATOR . "messages.txt";
 if (!empty($_REQUEST))
 {
    if (mb_strlen($_REQUEST["pseudo"],"UTF-8")<3 || mb_strlen($_REQUEST["mssge"],"UTF-8")<10)
@@ -17,7 +18,6 @@ if (!empty($_REQUEST))
                     'message' => $_REQUEST['mssge'],
                     'date'=> time()
     ];
-    $file = __DIR__ . DIRECTORY_SEPARATOR . "fichiers" .DIRECTORY_SEPARATOR . "messages.txt";
     if(!file_exists($file))
     {
         $resource  = fopen($file,'x+');
@@ -37,6 +37,24 @@ if (!empty($_REQUEST))
 require_once __DIR__ . DIRECTORY_SEPARATOR . "inclus" . DIRECTORY_SEPARATOR ."form.php";
 ?>
 <h1>Vos messages : </h1>
+<div>
+    <?php
+    if (file_exists($file))
+    {
+        $resource = fopen($file,'r');
+        while($line = fgets($resource))
+        {
+            $line = json_decode($line,true);
+            $date = date('D d M o / H : i : s',$line['date']);
+        echo("
+            <P>
+                <strong>{$line['username']}</strong> <em>{$date} </em>  <br> <p>{$line['message']}</p>
+            </P>");
+          
+        }
+    }
+    ?>
+</div>
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . "inclus" . DIRECTORY_SEPARATOR ."foot.php";
 
